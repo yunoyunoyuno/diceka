31658af0cc30dba8be4ef2d5c7fa5eb92ec1a919
yunoyunoyuno <Moreseus@outlook.com>
2019-12-26 16:19:37 +0700

    Updates

---
+++ precache-manifest.d958cf4ce31cd295364840da275c5f3e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f692758a1825c1fd54ef01c32690680",
+    "url": "/diceka/index.html"
+  },
+  {
+    "revision": "7c932ebd61299d2087d4",
+    "url": "/diceka/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "53f21d0749f70b992d55",
+    "url": "/diceka/static/css/main.fb757e24.chunk.css"
+  },
+  {
+    "revision": "7c932ebd61299d2087d4",
+    "url": "/diceka/static/js/2.591fb7c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/diceka/static/js/2.591fb7c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53f21d0749f70b992d55",
+    "url": "/diceka/static/js/main.9e9214b8.chunk.js"
+  },
+  {
+    "revision": "58714e7646aeed752a71",
+    "url": "/diceka/static/js/runtime-main.b7e71812.js"
+  },
+  {
+    "revision": "b61bab359887250f9f1715f74681307e",
+    "url": "/diceka/static/media/logo.b61bab35.png"
+  },
+  {
+    "revision": "5360fa445a601153fbe10118be4a1858",
+    "url": "/diceka/static/media/math.5360fa44.jpg"
+  }
+]);
